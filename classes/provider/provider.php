b0414aeffa5b0fffc9208e5e6350357f1ecb57b3
Mike Churchward <mike@remote-learner.net>
2016-09-07 09:24:49 -0400

    POET-85 - Fixing erroneously changed code from merge.

--- classes/provider/provider.php
+++ classes/provider/provider.php
@@ -97,8 +97,8 @@ class provider {
      */
     public function implementation() {
         $implarr = [
-            'providername' => $this->providername,
-            'providerurl' => $this->providerurl,
+            'provider_name' => $this->providername,
+            'provider_url' => $this->providerurl,
             'endpoints' => [],
         ];
         foreach ($this->endpoints as $endpoint) {
